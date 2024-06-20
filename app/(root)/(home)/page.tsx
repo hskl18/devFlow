069a7477a377b26feed69171e89a185be5a18e2c
hskl18 <thb182003@gmail.com>
2024-06-20 12:15:02 -0700

    add themeprovider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full">
+      <div className=" float-right">
+        <UserButton />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
